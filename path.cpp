62da67b5ad814fa8255e6ff41032c37774585e33
patrick <patrick@andestech.com>
2024-04-04 23:58:46 +0800

    Use implementation from Process.inc
    The implementation in Path.inc needs SmallVector and it will
    bring in much dependency, so use similar implementation.
    
    The implementation of UTF16ToUTF8 is from Path.inc.
    
    Note:
    1. All functions need to be modified manually.
    2. UTF16ToUTF8 is not tested.

--- path.cpp
+++ path.cpp
@@ -2,8 +2,47 @@
 #include <unistd.h>
 
 #include <string>
+
 #ifdef __APPLE__
 #include <mach-o/dyld.h>
+#endif
+
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+
+#if defined(_WIN32)
+static
+int UTF16ToCodePage(unsigned codepage, const wchar_t *utf16,
+                                size_t utf16_len,
+                                char *converted) {
+  if (utf16_len) {
+    // Get length.
+    int len = ::WideCharToMultiByte(codepage, 0, utf16, utf16_len, converted,
+                                    0, NULL, NULL);
+
+    if (len == 0) {
+      return -1;
+    }
+
+    // Now do the actual conversion.
+    len = ::WideCharToMultiByte(codepage, 0, utf16, utf16_len, converted,
+                                MAX_PATH, NULL, NULL);
+
+    if (len == 0) {
+      return -1;
+    }
+    // Make the new string null terminated.
+    converted[len] = '\0';
+  }
+
+  return 0;
+}
+
+int UTF16ToUTF8(const wchar_t *utf16, size_t utf16_len,
+                            char *utf8) {
+  return UTF16ToCodePage(CP_UTF8, utf16, utf16_len, utf8);
+}
 #endif
 
 /// GetMainExecutable - Return the path to the main executable, given the
@@ -54,32 +93,34 @@ std::string getMainExecutableImpl(const char *argv0, void *MainAddr) {
   // Fall back to the classical detection.
   //if (getprogpath(exe_path, argv0))
   //  return exe_path;
-#elif defined(__MINGW32__)
-  SmallVector<wchar_t, MAX_PATH> PathName;
-  PathName.resize_for_overwrite(PathName.capacity());
-  DWORD Size = ::GetModuleFileNameW(NULL, PathName.data(), PathName.size());
+#elif defined(_WIN32)
+  // The first argument may contain just the name of the executable (e.g.,
+  // "clang") rather than the full path, so swap it with the full path.
+  wchar_t ModuleName[MAX_PATH];
+  size_t Length = ::GetModuleFileNameW(NULL, ModuleName, MAX_PATH);
+  if (Length == 0 || Length == MAX_PATH) {
+    return "";
+  }
 
-  // A zero return value indicates a failure other than insufficient space.
-  if (Size == 0)
+  // If the first argument is a shortened (8.3) name (which is possible even
+  // if we got the module name), the driver will have trouble distinguishing it
+  // (e.g., clang.exe v. clang++.exe), so expand it now.
+  Length = GetLongPathNameW(ModuleName, ModuleName, MAX_PATH);
+  if (Length == 0)
+    return "";
+  if (Length > MAX_PATH) {
+    // We're not going to try to deal with paths longer than MAX_PATH, so we'll
+    // treat this as an error.  GetLastError() returns ERROR_SUCCESS, which
+    // isn't useful, so we'll hardcode an appropriate error value.
+    return "";
+  }
+
+  char ModuleNameUTF8[MAX_PATH];
+  int EC = UTF16ToUTF8(ModuleName, Length, ModuleNameUTF8);
+  if (EC)
     return "";
 
-  // Insufficient space is determined by a return value equal to the size of
-  // the buffer passed in.
-  if (Size == PathName.capacity())
-    return "";
-
-  // On success, GetModuleFileNameW returns the number of characters written to
-  // the buffer not including the NULL terminator.
-  PathName.truncate(Size);
-
-  // Convert the result from UTF-16 to UTF-8.
-  SmallVector<char, MAX_PATH> PathNameUTF8;
-  if (UTF16ToUTF8(PathName.data(), PathName.size(), PathNameUTF8))
-    return "";
-
-  //llvm::sys::path::make_preferred(PathNameUTF8);
-  std::replace(PathNameUTF8.begin(), PathNameUTF8.end(), '\\', '/');
-  return std::string(PathNameUTF8.data());
+  return std::string(ModuleNameUTF8);
 #else
 #error GetMainExecutable is not implemented on this host yet.
 #endif
