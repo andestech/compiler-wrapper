9ab523d7d32ac515ff03bb78b098e733eee29eab
Shao-Chung Wang <scwang@andestech.com>
2025-11-26 08:24:28 +0800

    Add Licenses

--- path.cpp
+++ path.cpp
@@ -1,3 +1,11 @@
+//===- path.cpp - Unix Path Implementation  -------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include <limits.h>
 #include <unistd.h>
 
